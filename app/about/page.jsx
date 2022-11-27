db5609fecc6f2ec1d03235ad8f6a2aadd180434a
ishan-me <ishan02016@gmail.com>
2022-11-27 15:19:00 +0545

    adding some slide in animation to image

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -23,7 +23,7 @@ const images = {
     opacity: 1,
     x: 0,
     transition: {
-      duration: 1,
+      duration: 1
     },
   },
 };
@@ -34,59 +34,59 @@ export default function Aboutpage() {
       <h1>
         <Navbar />
         <PageWrapper>
-          <div class="bg-white">
-            <div class="mx-auto grid max-w-2xl grid-cols-1 items-center gap-y-16 gap-x-8 py-24 sm:py-20 lg:max-w-7xl lg:grid-cols-2 lg:px-5">
+          <div className="bg-white">
+            <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-y-16 gap-x-8 py-24 sm:py-20 lg:max-w-7xl lg:grid-cols-2 lg:px-5">
               <div>
-                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                   Technical Specifications
                 </h2>
-                <p class="mt-4 text-gray-500">
+                <p className="mt-4 text-gray-500">
                   The walnut wood card tray is precision milled to perfectly fit
                   a stack of Focus cards. The powder coated steel divider
                   separates active cards from new ones, or can be used to
                   archive important task lists.
                 </p>
 
-                <dl class="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
-                  <div class="border-t border-gray-200 pt-4">
-                    <dt class="font-medium text-gray-900">Origin</dt>
-                    <dd class="mt-2 text-sm text-gray-500">
+                <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+                  <div className="border-t border-gray-200 pt-4">
+                    <dt className="font-medium text-gray-900">Origin</dt>
+                    <dd className="mt-2 text-sm text-gray-500">
                       Designed by Good Goods, Inc.
                     </dd>
                   </div>
 
-                  <div class="border-t border-gray-200 pt-4">
-                    <dt class="font-medium text-gray-900">Material</dt>
-                    <dd class="mt-2 text-sm text-gray-500">
+                  <div className="border-t border-gray-200 pt-4">
+                    <dt className="font-medium text-gray-900">Material</dt>
+                    <dd className="mt-2 text-sm text-gray-500">
                       Solid walnut base with rare earth magnets and powder
                       coated steel card cover
                     </dd>
                   </div>
 
-                  <div class="border-t border-gray-200 pt-4">
-                    <dt class="font-medium text-gray-900">Dimensions</dt>
-                    <dd class="mt-2 text-sm text-gray-500">
+                  <div className="border-t border-gray-200 pt-4">
+                    <dt className="font-medium text-gray-900">Dimensions</dt>
+                    <dd className="mt-2 text-sm text-gray-500">
                       6.25&quot; x 3.55&quot; x 1.15&quot;
                     </dd>
                   </div>
 
-                  <div class="border-t border-gray-200 pt-4">
-                    <dt class="font-medium text-gray-900">Finish</dt>
-                    <dd class="mt-2 text-sm text-gray-500">
+                  <div className="border-t border-gray-200 pt-4">
+                    <dt className="font-medium text-gray-900">Finish</dt>
+                    <dd className="mt-2 text-sm text-gray-500">
                       Hand sanded and finished with natural oil
                     </dd>
                   </div>
 
-                  <div class="border-t border-gray-200 pt-4">
-                    <dt class="font-medium text-gray-900">Includes</dt>
-                    <dd class="mt-2 text-sm text-gray-500">
+                  <div className="border-t border-gray-200 pt-4">
+                    <dt className="font-medium text-gray-900">Includes</dt>
+                    <dd className="mt-2 text-sm text-gray-500">
                       Wood card tray and 3 refill packs
                     </dd>
                   </div>
 
-                  <div class="border-t border-gray-200 pt-4">
-                    <dt class="font-medium text-gray-900">Considerations</dt>
-                    <dd class="mt-2 text-sm text-gray-500">
+                  <div className="border-t border-gray-200 pt-4">
+                    <dt className="font-medium text-gray-900">Considerations</dt>
+                    <dd className="mt-2 text-sm text-gray-500">
                       Made from natural materials. Grain and color vary with
                       each item.
                     </dd>
@@ -97,29 +97,29 @@ export default function Aboutpage() {
               variants={variants}
               initial="hidden"
               animate="show"
-              class="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
+              className="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
                 <motion.img
                   src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg"
                   alt="Walnut card tray with white powder coated steel divider and 3 punchout holes."
-                  class="rounded-lg bg-gray-100"
+                  className="rounded-lg bg-gray-100"
                   variants={images}
                 />
                 <motion.img
                   src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg"
                   alt="Top down view of walnut card tray with embedded magnets and card groove."
-                  class="rounded-lg bg-gray-100"
+                  className="rounded-lg bg-gray-100"
                   variants={images}
                 />
                 <motion.img
                   src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg"
                   alt="Side of walnut card tray with card groove and recessed card area."
-                  class="rounded-lg bg-gray-100"
+                  className="rounded-lg bg-gray-100"
                   variants={images}
                 />
                 <motion.img
                   src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg"
                   alt="Walnut card tray filled with cards and card angled in dedicated groove."
-                  class="rounded-lg bg-gray-100"
+                  className="rounded-lg bg-gray-100"
                   variants={images}
                 />
               </motion.div>
--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,14 +1,192 @@
 "use client";
 
+import { motion } from "framer-motion";
 import { Navbar } from "./../navbar";
 import { PageWrapper } from "./../page-wrapper";
+
+const variants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const images = {
+  hidden: {
+    opacity: 0,
+    y: 30,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+    },
+  },
+};
 
 export default function Productpage() {
   return (
     <div className="container mx-auto">
       <h1>
         <Navbar />
-        <PageWrapper>Product page</PageWrapper>
+        <PageWrapper>
+          <div className="bg-white">
+            <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-y-16 gap-x-8 pt-24 sm:py-10 lg:max-w-7xl lg:grid-cols-2 lg:px-0">
+              <div>
+                <div class="w-full">
+                  <div class="mb-12 max-w-[620px] lg:mb-20">
+                    <span class="mb-2 block text-lg font-semibold text-primary">
+                      All New
+                    </span>
+                    <h2 class="mb-4 text-3xl font-bold text-dark sm:text-4xl md:text-[42px]">
+                      All Products
+                    </h2>
+                    <p class="text-lg leading-relaxed text-body-color sm:text-xl sm:leading-relaxed">
+                      There are many variations of passages of Lorem Ipsum
+                      available but the majority have suffered alteration in
+                      some form.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <motion.div
+              variants={variants}
+              initial="hidden"
+              animate="show"
+              class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
+            >
+              <motion.div variants={images} class="group relative">
+                <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                  <img
+                    src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+                    alt="Front of men&#039;s Basic Tee in black."
+                    class="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div class="mt-4 flex justify-between">
+                  <div>
+                    <h3 class="text-sm text-gray-700">
+                      <a href="#">
+                        <span
+                          aria-hidden="true"
+                          class="absolute inset-0"
+                        ></span>
+                        Basic Tee
+                      </a>
+                    </h3>
+                    <p class="mt-1 text-sm text-gray-500">Black</p>
+                  </div>
+                  <p class="text-sm font-medium text-gray-900">$35</p>
+                </div>
+              </motion.div>
+
+              <motion.div variants={images} class="group relative">
+                <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                  <img
+                    src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+                    alt="Front of men&#039;s Basic Tee in black."
+                    class="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div class="mt-4 flex justify-between">
+                  <div>
+                    <h3 class="text-sm text-gray-700">
+                      <a href="#">
+                        <span
+                          aria-hidden="true"
+                          class="absolute inset-0"
+                        ></span>
+                        Basic Tee
+                      </a>
+                    </h3>
+                    <p class="mt-1 text-sm text-gray-500">Black</p>
+                  </div>
+                  <p class="text-sm font-medium text-gray-900">$35</p>
+                </div>
+              </motion.div>
+
+              <motion.div variants={images} class="group relative">
+                <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                  <img
+                    src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+                    alt="Front of men&#039;s Basic Tee in black."
+                    class="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div class="mt-4 flex justify-between">
+                  <div>
+                    <h3 class="text-sm text-gray-700">
+                      <a href="#">
+                        <span
+                          aria-hidden="true"
+                          class="absolute inset-0"
+                        ></span>
+                        Basic Tee
+                      </a>
+                    </h3>
+                    <p class="mt-1 text-sm text-gray-500">Black</p>
+                  </div>
+                  <p class="text-sm font-medium text-gray-900">$35</p>
+                </div>
+              </motion.div>
+
+              <motion.div variants={images} class="group relative">
+                <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                  <img
+                    src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+                    alt="Front of men&#039;s Basic Tee in black."
+                    class="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div class="mt-4 flex justify-between">
+                  <div>
+                    <h3 class="text-sm text-gray-700">
+                      <a href="#">
+                        <span
+                          aria-hidden="true"
+                          class="absolute inset-0"
+                        ></span>
+                        Basic Tee
+                      </a>
+                    </h3>
+                    <p class="mt-1 text-sm text-gray-500">Black</p>
+                  </div>
+                  <p class="text-sm font-medium text-gray-900">$35</p>
+                </div>
+              </motion.div>
+
+              <motion.div variants={images} class="group relative">
+                <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                  <img
+                    src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+                    alt="Front of men&#039;s Basic Tee in black."
+                    class="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div class="mt-4 flex justify-between">
+                  <div>
+                    <h3 class="text-sm text-gray-700">
+                      <a href="#">
+                        <span
+                          aria-hidden="true"
+                          class="absolute inset-0"
+                        ></span>
+                        Basic Tee
+                      </a>
+                    </h3>
+                    <p class="mt-1 text-sm text-gray-500">Black</p>
+                  </div>
+                  <p class="text-sm font-medium text-gray-900">$35</p>
+                </div>
+              </motion.div>
+            </motion.div>
+          </div>
+        </PageWrapper>
       </h1>
     </div>
   );
